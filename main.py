fd918c2388abf55ecba4178d22698adf2f81d2c3
Roderick Lan <rodericklan03@gmail.com>
2025-11-11 13:30:47 -0700

    updated main script to use voice detection

--- main.py
+++ main.py
@@ -25,18 +25,34 @@ stream = p.open(
 
 if __name__ == "__main__":
     ring_buffer = deque(maxlen=NUM_PADDING_CHUNKS)
+    is_triggered = False
     frames = []
     start = time.time()
     while True:
         data = stream.read(CHUNK, exception_on_overflow=False)
-        frames.append(np.frombuffer(data, np.int16).astype(np.float32) / 32768.0)
-        # todo: adaptive, record for as long as someone is talking
-        if time.time() - start > 5:
-            audio = np.concatenate(frames)
-            result = model.transcribe(audio, fp16=False)["text"]
-            # mel = whisper.log_mel_spectrogram(whisper.pad_or_trim(audio), n_mels=model.dims.n_mels).to(model.device)
-            # result = whisper.decode(model, mel, options).text
-            if result.strip():
-                print(f"{result}")
-            frames = []
-            start = time.time()
+        is_speech = vad.is_speech(data, RATE)
+        if not is_triggered:
+            ring_buffer.append((data, is_speech))
+            n_voiced = len([f for f, _is_speech in ring_buffer if _is_speech])
+            if n_voiced > .9 * ring_buffer.maxlen:
+                # talking 
+                is_triggered = True
+                frames.extend([f for f, _ in ring_buffer])
+                ring_buffer.clear()
+        else:
+            frames.append(data)
+            ring_buffer.append((data, is_speech))
+            n_unvoiced = len([f for f, _is_speech in ring_buffer if not _is_speech])
+            if n_unvoiced > 0.9 * ring_buffer.maxlen:
+                # stopped talking
+                is_triggered = False
+                audio = np.frombuffer(b''.join(frames), dtype=np.int16).astype(np.float32) / 32768.0
+                result = model.transcribe(audio, fp16=False)["text"]
+                # mel = whisper.log_mel_spectrogram(whisper.pad_or_trim(audio), n_mels=model.dims.n_mels).to(model.device)
+                # result = whisper.decode(model, mel, options).text
+                if result.strip():
+                    print(f"{result}")
+                frames = []
+                ring_buffer.clear()
+            if time.time() - start > 5:
+                start = time.time()
